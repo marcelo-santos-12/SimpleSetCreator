df5e372c770d62b8eb3b6b8981371108d554879c
TheCamilovisk <camilolgon@gmail.com>
2018-05-12 00:18:37 -0300

    Initial implementation complete

--- main_marker.py
+++ main_marker.py
@@ -15,24 +15,38 @@ import pandas as pd
 
 from matplotlib.widgets import RectangleSelector, Cursor
 
-import geometry as geo
 import utils
+
+def create_lists(bounds):
+    x1s = []
+    y1s = []
+    x2s = []
+    y2s = []
+
+    for (x, y, width, height) in bounds:
+        x1s.append(x)
+        y1s.append(y)
+        x2s.append(x+width)
+        y2s.append(y+height)
+
+    return x1s, y1s, x2s, y2s
 
 
 class ImageMarker:
 
-    def __init__(self, img, img_ext="png", output_folder="."):
+    def __init__(self, img, img_dir, img_name, output_folder="."):
 
         self._image = img
 
-        self._pos_out_dir = "{}/positives".format(output_folder)
-        self._neg_out_dir = "{}/negatives".format(output_folder)
+        self._img_dir = "{}/{}".format(output_folder, img_dir)
+        self._img_name = img_name
 
         self._patches = []
 
         self._figure, self._axis = plt.subplots(1)
 
-        self._keyset = {"quit": "q", "undo": "u", "skip": "s", "help": "h", "process": "p"}
+        self._keyset = {"quit": "q", "undo": "u", "skip": "s", "help": "h", \
+                "process": "p"}
         self._modifiers = {"shift", "control"}
 
         self._should_quit = False
@@ -126,101 +140,20 @@ class ImageMarker:
 
     def _create_csv(self):
 
+        if not self._patches:
+            return
+
         bounds = [(obj.get_bbox().bounds) for obj in self._patches]
 
-        x1s, y1s, x2s, y2s = _create_lists(bounds)
+        x1s, y1s, x2s, y2s = create_lists(bounds)
 
         raw_data = {"x_1": x1s, "y_1": y1s, "x_2": x2s, "y_2": y2s}
         df = pd.DataFrame(raw_data, columns=["x_1", "y_1", "x_2", "y_2", ])
 
-        ou_file = "{}/{}.csv".format(o_name, i+1, time_block)
+        utils.check_dir(self._img_dir)
+
+        ou_file = "{}/{}.csv".format(self._img_dir, self._img_name)
         df.to_csv(ou_file, index=False)
-
-    
-    def _create_lists(self, bounds):
-        x1s = []
-        y1s = []
-        x2s = []
-        y2s = []
-
-        for (x, y, width, height) in bounds:
-            x1s.append(x)
-            y1s.append(y)
-            x2s.append(x+width)
-            y2s.append(y+height)
-
-        return x1s, y1s, x2s, y2s
-
-
-    def _create_positive_samples(self, bboxes):
-
-        utils.check_dir(self._pos_out_dir)
-
-        rot_mats = []
-        rot_mats.append(cv2.getRotationMatrix2D( \
-                (self._sample_sz // 2, self._sample_sz // 2), 90, 1.0))
-        rot_mats.append(cv2.getRotationMatrix2D( \
-                (self._sample_sz // 2, self._sample_sz // 2), 180, 1.0))
-        rot_mats.append(cv2.getRotationMatrix2D( \
-                (self._sample_sz // 2, self._sample_sz // 2), 270, 1.0))
-
-        for box in bboxes:
-            p1 = box.tl
-            p2 = box.br
-
-            imgs = []
-            croped = self._image[int(p1.y):int(p2.y), int(p1.x):int(p2.x), :]
-            imgs.append(cv2.resize(croped, (self._sample_sz, self._sample_sz), \
-                    interpolation=cv2.INTER_AREA))
-            imgs.append(cv2.flip(imgs[0], 1))
-
-            for img in imgs:
-                tmp_name = utils.get_random_file_name(extension=self._img_ext)
-                cv2.imwrite("{}/{}".format(self._pos_out_dir, tmp_name), img)
-
-                for r_mat in rot_mats:
-                    tmp_name = utils.get_random_file_name(extension=self._img_ext)
-                    rotated = cv2.warpAffine(img, r_mat, (self._sample_sz, self._sample_sz))
-                    cv2.imwrite("{}/{}".format(self._pos_out_dir, tmp_name), rotated)
-
-                    resized = cv2.pyrUp(cv2.pyrDown(rotated))
-                    tmp_name = utils.get_random_file_name(extension=self._img_ext)
-                    cv2.imwrite("{}/{}".format(self._pos_out_dir, tmp_name), resized)
-
-
-    def _create_negative_samples(self, bboxes):
-
-        utils.check_dir(self._neg_out_dir)
-
-        img_res = cv2.pyrDown(cv2.pyrDown(self._image))
-        boxes_res = [b.resize(1/4) for b in bboxes]
-
-        for prob_b in self._create_boxes(img_res.shape[:2], boxes_res):
-            tl = prob_b.tl
-            br = prob_b.br
-
-            croped = img_res[int(tl.y):int(br.y), int(tl.x):int(br.x), :]
-            tmp_name = utils.get_random_file_name(extension=self._img_ext)
-            cv2.imwrite("{}/{}".format(self._neg_out_dir, tmp_name), croped)
-
-
-    def _create_boxes(self, img_dims, pos_boxes):
-        boxes = []
-
-        for y in range(0, img_dims[0]-self._sample_sz, self._sample_sz):
-            for x in range(0, img_dims[1]-self._sample_sz, self._sample_sz):
-                b = geo.BBox((x, y), (x+self._sample_sz, y+self._sample_sz))
-
-                valid = True
-
-                for p_b in pos_boxes:
-                    if p_b.intersect(b):
-                        valid = False
-
-                if valid is True:
-                    boxes.append(b)
-
-        return boxes
 
 
 if __name__ == "__main__":
@@ -232,18 +165,14 @@ if __name__ == "__main__":
     ap.add_argument("-ie", "--input_extension", required=False, \
             help="The extension type of the input images")
 
-    ap.add_argument("-oe", "--output_extension", required=False, \
-            help="The extension type of the output images")
-
     ap.add_argument("-o", "--output", required=False, \
-            help="The folder where the positive and negative samples will be stored")
+            help="The folder where the csvs folder will be stored.")
 
     args = vars(ap.parse_args())
 
     i_name = args["input"]
     o_name = "samples"
     i_ext = "png"
-    o_ext = "png"
 
     if args["input_extension"]:
         i_ext = args["input_extension"]
@@ -265,7 +194,10 @@ if __name__ == "__main__":
     for i, f in enumerate(files):
         print("Processing file: {} ({}/{})".format(f, i+1, n_files))
         image = cv2.imread(f)
-        creator = ImageMarker(image, img_ext=o_ext, output_folder=o_name)
+        splited_filename = f.split("/")
+        i_dir, i_nam = splited_filename[-2], splited_filename[-1].split(".")[0]
+        creator = ImageMarker(image, img_dir=i_dir, img_name=i_nam, \
+                output_folder=o_name)
 
         if creator.should_quit:
             print("Terminating...")
